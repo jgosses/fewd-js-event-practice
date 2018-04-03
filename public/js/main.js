769d9acfba75bc4408752189016b7d1dbb9c039f
Jimmy Gosses <gosses.jim@gmail.com>
2018-04-03 18:29:54 -0400

    fix spacing errors and missing semicolons

--- public/js/main.js
+++ public/js/main.js
@@ -1,21 +1,19 @@
 (function () {
 
-const button = document.querySelector('button');
+  const button = document.querySelector('button');
 
-  button.addEventListener('click',function(event1) {
-    button.classList.toggle('button-danger');
-});
+    button.addEventListener('click', function (event1) {
+      button.classList.toggle('button-danger');
+  });
 
+  const button2 = document.querySelector('.button-success');
+    button2.addEventListener('click', function (event2) {
+      button2.innerText = 'You Clicked Me';
+  });
 
-const button2 = document.querySelector('.button-success');
-  button2.addEventListener('click',function(event2) {
-    button2.innerText = 'You Clicked Me';
-  
-});
-
-const button3 = document.querySelector('.button-warning');
-  button3.addEventListener('click',function(event3) {
-    button2.innerText = 'button two'
-  })
+  const button3 = document.querySelector('.button-warning');
+    button3.addEventListener('click', function (event3) {
+      button2.innerText = 'button two';
+    });
 
 })();
