22c60fce09d07f9562f2f29fb76cbcf830e1aba0
Jimmy Gosses <gosses.jim@gmail.com>
2018-04-02 23:28:49 -0400

    change inner text of button two with click

--- public/js/main.js
+++ public/js/main.js
@@ -6,4 +6,18 @@ const button = document.querySelector('button');
     button.classList.toggle('button-danger');
 });
 
+
+const button2 = document.querySelector('.button-success');
+button2.addEventListener('click',function(event2) {
+  button2.innerText = 'You Clicked Me';
+  
+});
+
+
+
 })();
+
+/*var button2 = document.querySelector('button-success');
+
+  button2.addEventListener('click',function(event2) {
+    button2.innerText("You Clicked Me"); */
