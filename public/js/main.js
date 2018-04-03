46ac411c540cd76d809f5ad9c7c8cf5f31c19899
Jimmy Gosses <gosses.jim@gmail.com>
2018-04-02 23:55:00 -0400

    clean up js

--- public/js/main.js
+++ public/js/main.js
@@ -19,8 +19,3 @@ const button3 = document.querySelector('.button-warning');
   })
 
 })();
-
-/*var button2 = document.querySelector('button-success');
-
-  button2.addEventListener('click',function(event2) {
-    button2.innerText("You Clicked Me"); */
