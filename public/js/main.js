ae6f14938443dea0bf962e0612e8dde34ae42ff3
Jimmy Gosses <gosses.jim@gmail.com>
2018-04-02 23:34:31 -0400

    change inner text button two with button three click

--- public/js/main.js
+++ public/js/main.js
@@ -8,12 +8,15 @@ const button = document.querySelector('button');
 
 
 const button2 = document.querySelector('.button-success');
-button2.addEventListener('click',function(event2) {
-  button2.innerText = 'You Clicked Me';
+  button2.addEventListener('click',function(event2) {
+    button2.innerText = 'You Clicked Me';
   
 });
 
-
+const button3 = document.querySelector('.button-warning');
+  button3.addEventListener('click',function(event3) {
+    button2.innerText = 'button two'
+  })
 
 })();
 
