df195ab34173927a43a36b9b849dea438b19477f
Jimmy Gosses <gosses.jim@gmail.com>
2018-04-03 20:01:06 -0400

    reformat js syntax

--- public/js/main.js
+++ public/js/main.js
@@ -1,19 +1,17 @@
 (function () {
-
   const button = document.querySelector('button');
-
-    button.addEventListener('click', function (event1) {
-      button.classList.toggle('button-danger');
-  });
-
   const button2 = document.querySelector('.button-success');
-    button2.addEventListener('click', function (event2) {
-      button2.innerText = 'You Clicked Me';
+  const button3 = document.querySelector('.button-warning');
+
+  button.addEventListener('click', function (event1) {
+    button.classList.toggle('button-danger');
   });
 
-  const button3 = document.querySelector('.button-warning');
-    button3.addEventListener('click', function (event3) {
-      button2.innerText = 'button two';
-    });
+  button2.addEventListener('click', function (event2) {
+    button2.innerText = 'You Clicked Me';
+  });
 
+  button3.addEventListener('click', function (event3) {
+    button2.innerText = 'button two';
+  });
 })();
